b511c676c494b6157a445bd36fa66df086ace910
LucasBalbueno <balbuenolucas04@gmail.com>
2024-10-22 18:02:05 -0300

    Estrutura inicial da api

---
+++ src/main/java/com/example/Innovation_academy_api/InnovationAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Innovation_academy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Innovation_academy_api/InnovationAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Innovation_academy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
