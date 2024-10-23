6cf08f370fa47964ff588100f2069681488a5bbe
LucasBalbueno <balbuenolucas04@gmail.com>
2024-10-23 15:43:03 -0300

    Criando configuração do Spring Security e JWTEncode e Decoder

---
+++ src/main/java/Innovation/Academy/Innovation_academy_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package Innovation.Academy.Innovation_academy_api.repository;
+
+import Innovation.Academy.Innovation_academy_api.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
